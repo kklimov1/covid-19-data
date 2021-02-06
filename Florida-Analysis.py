430494f06af2cfafef0c24763d31594bf450b59e
Kirill Klimov <kirillvlklimov@gmail.com>
2021-02-06 11:33:30 -0800

    created a python notebook

--- Florida-Analysis.py
+++ Florida-Analysis.py
@@ -1,3 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+
+#Get our US State data supplied by The New York Times.
+state_data =pd.read_csv('~/Code/covid-19-data/us-states.csv')
+#print(state_data.loc[state_data['state']=='Florida'])
